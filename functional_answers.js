19ac7e8d90efd994263d006dab1b824624e8c1c6
Yuriy Belyayev <2215@paso.ru>
2022-06-29 11:27:38 +0400

    prettier

--- functional_answers.js
+++ functional_answers.js
@@ -12,10 +12,10 @@ console.log(addTwo(10));
 
 // Ответ
 
-const addS = (word) => word + 's';
+const addS = (word) => word + "s";
 
-console.log(addS('pizza'));
-console.log(addS('bagel'));
+console.log(addS("pizza"));
+console.log(addS("bagel"));
 
 // Challenge 3
 // Create a function called map that takes two inputs:
@@ -27,16 +27,15 @@ console.log(addS('bagel'));
 
 const multiplyByTwo = (num) => num * 2;
 const map = (array, callback) => {
-    const res = [];
-    for (let i=0; i < array.length; i++) {
-        res.push((callback(array[i])));
-    }
-    return res;
-}
-console.log(map([1,2,3,4,5], multiplyByTwo)); //-> [2,4,6,8,10]
+  const res = [];
+  for (let i = 0; i < array.length; i++) {
+    res.push(callback(array[i]));
+  }
+  return res;
+};
+console.log(map([1, 2, 3, 4, 5], multiplyByTwo)); //-> [2,4,6,8,10]
 console.log(multiplyByTwo(1)); //-> 2
 console.log(multiplyByTwo(2)); //-> 4
-
 
 // Challenge 4
 // The function forEach takes an array and a callback, and runs the callback on each element of the array. forEach does not return anything.
@@ -44,75 +43,79 @@ console.log(multiplyByTwo(2)); //-> 4
 // Ответ
 
 const forEach = (array, callback) => {
-    for (let i = 0; i < array.length; i++) {
-        callback(array[i]);
-    }
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i]);
+  }
 };
-let alphabet = '';
-const letters = ['a', 'b', 'c', 'd'];
-forEach(letters, char => alphabet += char);
-console.log(alphabet);   //prints 'abcd'
+let alphabet = "";
+const letters = ["a", "b", "c", "d"];
+forEach(letters, (char) => (alphabet += char));
+console.log(alphabet); //prints 'abcd'
 
 // Challenge 5
 // For this challenge, you're going to rebuild map as mapWith. This time you're going to use forEach inside of mapWith instead of using a for loop.
 
 // Ответ
 const mapwith = (array, callback) => {
-    const res = [];
-    forEach(array, el => res.push(callback(el)));
-    return res;
-}
-console.log(mapwith([1,2,3,4,5], multiplyByTwo)); //-> [2,4,6,8,10]
+  const res = [];
+  forEach(array, (el) => res.push(callback(el)));
+  return res;
+};
+console.log(mapwith([1, 2, 3, 4, 5], multiplyByTwo)); //-> [2,4,6,8,10]
 console.log(multiplyByTwo(1)); //-> 2
 console.log(multiplyByTwo(2)); //-> 4
 
 // Challenge 6
 // The function reduce takes an array and reduces the elements to a single value. For example it can sum all the numbers, multiply them, or any operation that you can put into a function.
 // const nums = [4, 1, 3];
-// const add = (a, b) => a + b; 
+// const add = (a, b) => a + b;
 // reduce(nums, add, 0);   //-> 8
 // Here's how it works. The function has an "accumulator value" which starts as the initialValue and accumulates the output of each loop. The array is iterated over, passing the accumulator and the next array element as arguments to the callback. The callback's return value becomes the new accumulator value. The next loop executes with this new accumulator value. In the example above, the accumulator begins at 0. add(0,4) is called. The accumulator's value is now 4. Then add(4, 1) to make it 5. Finally add(5, 3) brings it to 8, which is returned.
 
 // Ответ
 
 const reduce = (array, callback, initialValue) => {
-    let acc = initialValue;
-    for (let i=0; i < array.length; i++) {
-        acc = callback(acc, array[i]);
-    }
-    return acc;
+  let acc = initialValue;
+  for (let i = 0; i < array.length; i++) {
+    acc = callback(acc, array[i]);
+  }
+  return acc;
 };
 
 const nums = [4, 1, 3];
-const add = (a, b) => a + b; 
-console.log(reduce(nums, add, 0));   //-> 8
-
+const add = (a, b) => a + b;
+console.log(reduce(nums, add, 0)); //-> 8
 
 // Challenge 7
 // Construct a function intersection that compares input arrays and returns a new array with elements found in all of the inputs. BONUS: Use reduce!
 
 // Ответ Вариант 1
 
-const intersection = (ar1,ar2,ar3) => {
-    const reduce = ar1.reduce((acc,cur) => {
-        if (ar2.includes(cur) && ar3.includes(cur)) {
-            return acc.concat(cur);
-        } else {
-            return acc;
-        }}, [])
-        return reduce;
+const intersection = (ar1, ar2, ar3) => {
+  const reduce = ar1.reduce((acc, cur) => {
+    if (ar2.includes(cur) && ar3.includes(cur)) {
+      return acc.concat(cur);
+    } else {
+      return acc;
     }
-console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20])); // should log: [5, 15]
+  }, []);
+  return reduce;
+};
+console.log(
+  intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20])
+); // should log: [5, 15]
 
 // Ответ Вариант 2
 
 const intersection = (...arrays) => {
-    return arrays.reduce((acc,cur) => {
-    return acc.filter(el => cur.includes(el));
-    })
-  }
+  return arrays.reduce((acc, cur) => {
+    return acc.filter((el) => cur.includes(el));
+  });
+};
 
-console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20])); // should log: [5, 15]
+console.log(
+  intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20])
+); // should log: [5, 15]
 
 // Challenge 8
 // Construct a function union that compares input arrays and returns a new array that contains all elements. If there are duplicate elements, only add it once to the new array. Preserve the order of the elements starting from the first element of the first input array. BONUS: Use reduce!
@@ -120,8 +123,8 @@ console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20])
 // Ответ
 
 const union = (...arrays) => {
-    return arrays.reduce((acc, cur) => [...new Set(acc.concat(cur))]);
-}
+  return arrays.reduce((acc, cur) => [...new Set(acc.concat(cur))]);
+};
 
 console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
 // should log: [5, 10, 15, 88, 1, 7, 100]
@@ -132,15 +135,21 @@ console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
 // Ответ
 
 const objOfMatches = (array1, array2, callback) => {
-    return array1.reduce((acc, cur, index) => {
-        if (callback(cur) == array2[index]) {
-            acc[cur] = array2[index]
-        }
-        return acc;
-    }, {}) 
+  return array1.reduce((acc, cur, index) => {
+    if (callback(cur) == array2[index]) {
+      acc[cur] = array2[index];
+    }
+    return acc;
+  }, {});
 };
 
-console.log(objOfMatches(['hi', 'howdy', 'bye', 'later', 'hello'], ['HI', 'Howdy', 'BYE', 'LATER', 'hello'], (str) => str.toUpperCase()));
+console.log(
+  objOfMatches(
+    ["hi", "howdy", "bye", "later", "hello"],
+    ["HI", "Howdy", "BYE", "LATER", "hello"],
+    (str) => str.toUpperCase()
+  )
+);
 // should log: { hi: 'HI', bye: 'BYE', later: 'LATER' }
 
 // Challenge 10
@@ -149,32 +158,49 @@ console.log(objOfMatches(['hi', 'howdy', 'bye', 'later', 'hello'], ['HI', 'Howdy
 // Ответ Вариант 1
 
 const multiMap = (arrVals, arrCallbacks) => {
-    let acc = [];
-    const obj = {};
-    for (let i=0; i < arrVals.length; i++) {
-        for (let j=0; j < arrCallbacks.length; j++) {
-            acc.push(arrCallbacks[j](arrVals[i]));
-            }
-            obj[arrVals[i]] = acc;
-            acc = [];
+  let acc = [];
+  const obj = {};
+  for (let i = 0; i < arrVals.length; i++) {
+    for (let j = 0; j < arrCallbacks.length; j++) {
+      acc.push(arrCallbacks[j](arrVals[i]));
     }
-    return obj;
+    obj[arrVals[i]] = acc;
+    acc = [];
+  }
+  return obj;
 };
 
-console.log(multiMap(['catfood', 'glue', 'beer'], [(str) => str.toUpperCase(), (str) => str[0].toUpperCase() + str.slice(1).toLowerCase(), (str) => str + str]));
+console.log(
+  multiMap(
+    ["catfood", "glue", "beer"],
+    [
+      (str) => str.toUpperCase(),
+      (str) => str[0].toUpperCase() + str.slice(1).toLowerCase(),
+      (str) => str + str,
+    ]
+  )
+);
 // should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
 
 // Ответ Вариант 2
 
 const multiMap = (arrVals, arrCallbacks) => {
-    const res = arrVals.reduce((acc, cur) => {
-        acc[cur] = arrCallbacks.map((el) => el(cur));
-                return acc;
-                }
-    , {})
-    return res;
-}
-console.log(multiMap(['catfood', 'glue', 'beer'], [(str) => str.toUpperCase(), (str) => str[0].toUpperCase() + str.slice(1).toLowerCase(), (str) => str + str]));
+  const res = arrVals.reduce((acc, cur) => {
+    acc[cur] = arrCallbacks.map((el) => el(cur));
+    return acc;
+  }, {});
+  return res;
+};
+console.log(
+  multiMap(
+    ["catfood", "glue", "beer"],
+    [
+      (str) => str.toUpperCase(),
+      (str) => str[0].toUpperCase() + str.slice(1).toLowerCase(),
+      (str) => str + str,
+    ]
+  )
+);
 // should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
 
 // Challenge 11
@@ -183,7 +209,7 @@ console.log(multiMap(['catfood', 'glue', 'beer'], [(str) => str.toUpperCase(), (
 // Ответ
 
 const commutative = (func1, func2, value) => {
-    return (func1(func2(value)) == func2(func1(value))) 
+  return func1(func2(value)) == func2(func1(value));
 };
 
 // /*** Uncomment these to check your work! ***/
@@ -200,37 +226,37 @@ const commutative = (func1, func2, value) => {
 // Ответ Вариант 1
 
 const objFilter = (obj, callback) => {
-    const asArray = Object.entries(obj);
-    const filtered = asArray.filter(([key,val]) => val == callback((key)));
-    const objNew = Object.fromEntries(filtered);
-    return objNew;
+  const asArray = Object.entries(obj);
+  const filtered = asArray.filter(([key, val]) => val == callback(key));
+  const objNew = Object.fromEntries(filtered);
+  return objNew;
 };
 
 const startingObj = {};
 startingObj[6] = 3;
 startingObj[2] = 1;
 startingObj[12] = 4;
-const half = n => n / 2;
+const half = (n) => n / 2;
 console.log(objFilter(startingObj, half)); // should log: { 2: 1, 6: 3 }
 
 // Ответ Вариант 2 (с reduce)
 
 const objFilter = (obj, callback) => {
-    const asArray = Object.entries(obj);
-    const res = asArray.reduce((acc, cur) => {
-        if (callback(cur[0]) == cur[1]) {
-            acc[cur[0]] = cur[1]
-        }
-        return acc;
-    }, {})
-    return res;
+  const asArray = Object.entries(obj);
+  const res = asArray.reduce((acc, cur) => {
+    if (callback(cur[0]) == cur[1]) {
+      acc[cur[0]] = cur[1];
+    }
+    return acc;
+  }, {});
+  return res;
 };
 
 const startingObj = {};
 startingObj[6] = 3;
 startingObj[2] = 1;
 startingObj[12] = 4;
-const half = n => n / 2;
+const half = (n) => n / 2;
 console.log(objFilter(startingObj, half)); // should log: { 2: 1, 6: 3 }
 
 // Challenge 13
@@ -239,15 +265,14 @@ console.log(objFilter(startingObj, half)); // should log: { 2: 1, 6: 3 }
 // Ответ
 
 const rating = (arrOfFuncs, value) => {
-    const res = arrOfFuncs.reduce((acc, cur) => {
-        if (cur(value)) {
-            acc = acc + 1;
-        } 
-        return acc;
-    }, 0);
-    return (res/arrOfFuncs.length * 100);
+  const res = arrOfFuncs.reduce((acc, cur) => {
+    if (cur(value)) {
+      acc = acc + 1;
+    }
+    return acc;
+  }, 0);
+  return (res / arrOfFuncs.length) * 100;
 };
-
 
 // /*** Uncomment these to check your work! ***/
 // const isEven = n => n % 2 === 0;
@@ -264,29 +289,29 @@ const rating = (arrOfFuncs, value) => {
 // Ответ Вариант 1
 
 const pipe = (arrOfFuncs, value) => {
-    return arrOfFuncs.reduce((acc, cur) => cur(acc), value)
-    }
+  return arrOfFuncs.reduce((acc, cur) => cur(acc), value);
+};
 
-const capitalize = str => str.toUpperCase();
-const addLowerCase = str => str + str.toLowerCase();
-const repeat = str => str + str;
+const capitalize = (str) => str.toUpperCase();
+const addLowerCase = (str) => str + str.toLowerCase();
+const repeat = (str) => str + str;
 const capAddlowRepeat = [capitalize, addLowerCase, repeat];
-console.log(pipe(capAddlowRepeat, 'cat')); // should log: 'CATcatCATcat'
+console.log(pipe(capAddlowRepeat, "cat")); // should log: 'CATcatCATcat'
 
 // Ответ Вариант 2
 
 const pipe = (arrOfFuncs, value) => {
-    for (let i=0; i < arrOfFuncs.length; i++) {
+  for (let i = 0; i < arrOfFuncs.length; i++) {
     value = arrOfFuncs[i](value);
-    }
-    return value;
-}
+  }
+  return value;
+};
 
-const capitalize = str => str.toUpperCase();
-const addLowerCase = str => str + str.toLowerCase();
-const repeat = str => str + str;
+const capitalize = (str) => str.toUpperCase();
+const addLowerCase = (str) => str + str.toLowerCase();
+const repeat = (str) => str + str;
 const capAddlowRepeat = [capitalize, addLowerCase, repeat];
-console.log(pipe(capAddlowRepeat, 'cat')); // should log: 'CATcatCATcat'
+console.log(pipe(capAddlowRepeat, "cat")); // should log: 'CATcatCATcat'
 
 // Challenge 15
 // Create a function highestFunc that accepts an object (which will contain functions) and a subject (which is any value). highestFunc should return the key of the object whose associated value (which will be a function) returns the largest number, when the subject is given as input.
@@ -294,23 +319,26 @@ console.log(pipe(capAddlowRepeat, 'cat')); // should log: 'CATcatCATcat'
 // Answer Variant A
 
 const highestFunc = (objOfFuncs, subject) => {
-    const arr = Object.entries(objOfFuncs);
-    let resFunc;
-    const res = arr.reduce((acc, cur) => {
-        resFunc = cur[1](subject);
-        if (resFunc > acc[1]) 
-        { acc[0] = cur[0];
-          acc[1] = resFunc;
-        }
-        return acc;
-    }, ['',0]);
-        return res[0];
+  const arr = Object.entries(objOfFuncs);
+  let resFunc;
+  const res = arr.reduce(
+    (acc, cur) => {
+      resFunc = cur[1](subject);
+      if (resFunc > acc[1]) {
+        acc[0] = cur[0];
+        acc[1] = resFunc;
+      }
+      return acc;
+    },
+    ["", 0]
+  );
+  return res[0];
 };
 
 const groupOfFuncs = {};
-groupOfFuncs.double = n => n * 2;
-groupOfFuncs.addTen = n => n + 10;
-groupOfFuncs.inverse = n => n * -1;
+groupOfFuncs.double = (n) => n * 2;
+groupOfFuncs.addTen = (n) => n + 10;
+groupOfFuncs.inverse = (n) => n * -1;
 
 console.log(highestFunc(groupOfFuncs, 5)); // should log: 'addTen'
 console.log(highestFunc(groupOfFuncs, 11)); // should log: 'double'
@@ -322,7 +350,7 @@ console.log(highestFunc(groupOfFuncs, -20)); // should log: 'inverse'
 
 // Answer
 
-createFunction = () => () => console.log('hello');
+createFunction = () => () => console.log("hello");
 const function1 = createFunction();
 // now we'll call the function we just created
 function1(); //should console.log('hello');
@@ -333,8 +361,8 @@ function1(); //should console.log('hello');
 // Solution
 
 const createFunctionPrinter = (input) => () => console.log(input);
-const printSample = createFunctionPrinter('sample');
-const printHello = createFunctionPrinter('hello')
+const printSample = createFunctionPrinter("sample");
+const printHello = createFunctionPrinter("hello");
 // now we'll call the functions we just created
 printSample(); //should console.log('sample');
 printHello(); //should console.log('hello');
@@ -344,25 +372,25 @@ printHello(); //should console.log('hello');
 // Uncomment those lines of code. Try to deduce the output before executing.
 
 const outer = () => {
-    let counter = 0; // this variable is outside incrementCounter's scope
-    const incrementCounter = () => {
-      counter++;
-      console.log('counter', counter);
-    }
-    return incrementCounter;
+  let counter = 0; // this variable is outside incrementCounter's scope
+  const incrementCounter = () => {
+    counter++;
+    console.log("counter", counter);
   };
-  
-  const willCounter = outer();
-  const jasCounter = outer();
-  
-  willCounter(); // 1
-  willCounter(); // 2
-  willCounter(); // 3
-  
-  jasCounter(); // 1
-  willCounter(); // 4
+  return incrementCounter;
+};
 
-  // Challenge 4
+const willCounter = outer();
+const jasCounter = outer();
+
+willCounter(); // 1
+willCounter(); // 2
+willCounter(); // 3
+
+jasCounter(); // 1
+willCounter(); // 4
+
+// Challenge 4
 // Now we are going to create a function addByX that returns a function that will add an input by x.
 
 // Solution
@@ -387,27 +415,27 @@ console.log(addByFour(10)); //should return 14
 // The Solution
 
 const once = (func) => {
-    let hasBeenRun = false;
-    let value;
-    const inner= (n) => {
-        if(hasBeenRun === false) { 
-          hasBeenRun = true
-            value = func(n);
-            return value; 
-        }else{ 
-            return value;
-    } 
-}
-return inner;
-}
+  let hasBeenRun = false;
+  let value;
+  const inner = (n) => {
+    if (hasBeenRun === false) {
+      hasBeenRun = true;
+      value = func(n);
+      return value;
+    } else {
+      return value;
+    }
+  };
+  return inner;
+};
 
-const addByTwo = n => n + 2;
+const addByTwo = (n) => n + 2;
 const onceFunc = once(addByTwo);
 
 // UNCOMMENT THESE TO TEST YOUR WORK!
-console.log(onceFunc(4));  //should log 6
-console.log(onceFunc(10));  //should log 6
-console.log(onceFunc(9001));  //should log 6
+console.log(onceFunc(4)); //should log 6
+console.log(onceFunc(10)); //should log 6
+console.log(onceFunc(9001)); //should log 6
 
 // Challenge 6
 // Write a function after that takes the number of times the callback needs to be called before being executed as the first parameter and the callback as the second parameter.
@@ -415,15 +443,15 @@ console.log(onceFunc(9001));  //should log 6
 // The Solution
 
 const after = (count, func) => {
-    let currentCount = 0;
-    const inner= () => {
-        currentCount++;
-        if(currentCount === 3) func();
-        } 
-        return inner;
-}
+  let currentCount = 0;
+  const inner = () => {
+    currentCount++;
+    if (currentCount === 3) func();
+  };
+  return inner;
+};
 
-const called = () => console.log('hello');
+const called = () => console.log("hello");
 const afterCalled = after(3, called);
 
 afterCalled(); // -> nothing is printed
