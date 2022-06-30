573185b6aac21f2bdb92edb58aa77b8bc06c3a0a
Yuriy Belyayev <2215@paso.ru>
2022-06-30 15:46:05 +0400

    closure 11

--- functional_answers.js
+++ functional_answers.js
@@ -538,3 +538,35 @@ const capLastAttempt1 = (str) => str.toUpperCase();
 const capLastAttempt2 = (str) => str.slice(0, -1) + str.slice(-1).toUpperCase();
 console.log(shouldCapitalizeLast(capLastAttempt1)); // should log: false
 console.log(shouldCapitalizeLast(capLastAttempt2)); // should log: true
+
+// Challenge 11
+// Create a function makeHistory that accepts a number (which will serve as a limit), and returns a function (that will accept a string). The returned function will save a history of the most recent "limit" number of strings passed into the returned function (one per invocation only). Every time a string is passed into the function, the function should return that same string with the word 'done' after it (separated by a space). However, if the string 'undo' is passed into the function, then the function should delete the last action saved in the history, and return that delted string with the word 'undone' after (separated by a space). If 'undo' is passed into the function and the function's history is empty, then the function should return the string 'nothing to undo'.
+
+// The Solution
+
+const makeHistory = (limit) => {
+  const history = [];
+  const inner = (str) => {
+    if (str === "undo") {
+      if (!history.length) return "nothing to undo";
+      return history.pop() + " undone";
+    }
+    if (history.length === limit) {
+      history.shift();
+    }
+    history.push(str);
+    return str + " done";
+  };
+  return inner;
+};
+
+// /*** Uncomment these to check your work! ***/
+const myActions = makeHistory(2);
+console.log(myActions("jump")); // should log: 'jump done'
+console.log(myActions("undo")); // should log: 'jump undone'
+console.log(myActions("walk")); // should log: 'walk done'
+console.log(myActions("code")); // should log: 'code done'
+console.log(myActions("pose")); // should log: 'pose done'
+console.log(myActions("undo")); // should log: 'pose undone'
+console.log(myActions("undo")); // should log: 'code undone'
+console.log(myActions("undo")); // should log: 'nothing to undo'
