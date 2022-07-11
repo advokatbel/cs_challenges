fe3044d9cd9db0c544299e2d39f1d559993e2f75
Yuriy Belyayev <2215@paso.ru>
2022-07-11 12:14:12 +0400

    mixins

--- oop.js
+++ oop.js
@@ -168,3 +168,30 @@ function adminFactory(name, score) {
 adminFunctionStore.sharePublicMessage = function () {
   console.log("Welcome users!");
 };
+
+// EXTENSION: Mixins
+// Mixins are a tool in object-oriented programming that allows objects to be given methods and properties beyond those provided by their inheritance. For this challenge, complete the missing code, giving all of the robotMixin properties to robotFido. Do this in a single line of code, without adding the properties individually.
+
+class Dog {
+  constructor() {
+    this.legs = 4;
+  }
+  speak() {
+    console.log("Woof!");
+  }
+}
+
+const robotMixin = {
+  skin: "metal",
+  speak: function () {
+    console.log(`I have ${this.legs} legs and am made of ${this.skin}`);
+  },
+};
+
+let robotFido = new Dog();
+
+// The Solution
+
+robotFido = Object.setPrototypeOf(robotFido, robotMixin);
+
+robotFido.speak(); // -> Logs "I am made of metal"
